69d5b0facb953fdf9c553fcd68fe7e0cc55e7316
CrepeGoat <awqatty.b@gmail.com>
2020-03-26 11:44:26 -0400

    refactored to use more built-in itertools functions -> reduced code size

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,7 +2,7 @@ import pytest
 from love_and_stats import core
 
 import bisect
-from itertools import permutations
+import itertools
 import statistics
 
 
@@ -20,20 +20,12 @@ def play_game(item_ranks, mar_list):
 def brute_force_expt_score(mar_list):
     return statistics.mean(
         play_game(item_ranks, mar_list)
-        for item_ranks in permutations(range(len(mar_list)))
+        for item_ranks in itertools.permutations(range(len(mar_list)))
     )
 
 
-def gen_mar_lists(N, suffix=()):
-    if N == 0:
-        yield suffix
-        return
-
-    yield from (
-        mar
-        for i in range(N+1)
-        for mar in gen_mar_lists(N-1, (i,)+suffix) 
-    )
+def gen_mar_lists(N):
+    return itertools.product(*(range(i+2) for i in range(N)))
 
 
 @pytest.mark.parametrize('some_mar_list', list(gen_mar_lists(4)))
