c9df6220b812b1085d861cd48c362a9ed62ca601
CrepeGoat <awqatty.b@gmail.com>
2020-03-26 00:24:28 -0400

    tmp: splitting beta calculation from optimal MAP calculation

--- love_and_stats/core.py
+++ love_and_stats/core.py
@@ -1,15 +1,28 @@
 
-
-def reviter_min_allowable_ranks(N):
+def reviter_betas(N):
     beta_expt = -1
-    i_hat = 0
+    n = N
+    
+    def increment_beta(i_hat):
+        nonlocal n
+        assert 0 <= i_hat <= n
 
-    yield i_hat
-    for n in range(N, 0, -1):
         beta_expt = (i_hat / n)*beta_expt + (
             (n-i_hat) / (n+1)
             + (1+1/(n+1)) * 0.5*(n*(n-1) - i_hat*(i_hat-1))
         )
+
+        n = n-1
+        return beta_expt
+
+
+def reviter_min_allowable_ranks(N):
+    beta_generator = reviter_betas(N)
+    i_hat = 0
+
+    yield i_hat
+    for n in range(N, 0, -1):
+        beta_expt = beta_generator(i_hat)
 
         i_hat = (beta_expt*(n+1) - (N-n))/(N+1)
         yield i_hat
