0dc2862b5b64cf1b2dd591a061028bfcaa0c4621
CrepeGoat <awqatty.b@gmail.com>
2020-04-12 00:38:00 -0400

    changed `test_mar_is_optimal` to cache optimal solution

--- tests/test_optimal.py
+++ tests/test_optimal.py
@@ -22,9 +22,14 @@ def test_opt_mar_is_feasible(opt_mar_list):
     assert all(i <= j for i, j in zip(opt_mar_list[:-1], opt_mar_list[1:]))
 
 
-@pytest.mark.parametrize('some_mar_list', list(utils.gen_mar_lists(4)))
-def test_mar_is_optimal(some_mar_list):
-    assert (
-        utils.expt_score(optimal.max_allowable_ranks(len(some_mar_list)))
-        <= utils.expt_score(some_mar_list)
-    )
+def make_test_mar_is_optimal(n):
+    opt_mar_list = optimal.max_allowable_ranks(n)
+    opt_score = utils.expt_score(opt_mar_list)
+
+    @pytest.mark.parametrize('some_mar_list', list(utils.gen_mar_lists(n)))
+    def test_mar_is_optimal(some_mar_list):
+        assert opt_score <= utils.expt_score(some_mar_list)
+
+    return test_mar_is_optimal
+
+test_mar_is_optimal = make_test_mar_is_optimal(6)
