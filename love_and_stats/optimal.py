1cf72f77b01bb7015d985f1fafc09651ab59f10b
CrepeGoat <awqatty.b@gmail.com>
2020-04-12 01:21:31 -0400

    added basic implementation for max-acceptable-ranks (MAR) calculator

--- love_and_stats/optimal.py
+++ love_and_stats/optimal.py
@@ -1,6 +1,19 @@
 
-def max_allowable_ranks(N):
-    result = ()
+def reviter_max_allowable_ranks(N):
+    beta_expt = N
+    for n in range(N, 0, -1):
+        m = N-n
+        i_hat = (beta_expt*(n+1) - m)/(N+1)
+        yield i_hat
 
+        beta_expt = (
+            ((n-i_hat) / n)*beta_expt
+            + (i_hat / (n*(n+1))) * (m + 0.5*(i_hat-1)*(N+1))
+        )
+
+
+def max_allowable_ranks(N):
+    result = tuple(reviter_max_allowable_ranks(N))[::-1]
+    
     assert len(result) == N
     return result
