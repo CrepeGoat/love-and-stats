a347b2c3ea2507511fadf0a3ed00252e26a63e6d
CrepeGoat <awqatty.b@gmail.com>
2020-04-14 21:41:30 -0400

    added setup file
    - updated .gitignore to exclude files commonly generated in setuptools

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='love-and-stats',
+    packages=['love_and_stats'],
+    python_requires='>=3.5',
+    install_requires=['numpy', 'matplotlib'],
+)
